ee39757989986d001442f9d80411518c24e0fd40
ryan ernst <ryan@ryanernstnyberg.com>
2021-05-28 15:27:59 +0200

    Update [last_name].js

--- src/pages/over/[last_name].js
+++ src/pages/over/[last_name].js
@@ -5,7 +5,29 @@ import VideoBg from '../../components/VideoBg'
 import Zoom from 'react-reveal/Zoom';
 import * as React from 'react';
 
-
+function CommaFormatted(amount) {
+	var delimiter = ","; // replace comma if desired
+	var a = amount.split('.',2)
+	var d = a[1];
+	var i = parseInt(a[0]);
+	if(isNaN(i)) { return ''; }
+	var minus = '';
+	if(i < 0) { minus = '-'; }
+	i = Math.abs(i);
+	var n = new String(i);
+	var a = [];
+	while(n.length > 3) {
+		var nn = n.substr(n.length-3);
+		a.unshift(nn);
+		n = n.substr(0,n.length-3);
+	}
+	if(n.length > 0) { a.unshift(n); }
+	n = a.join(delimiter);
+	if(d.length < 1) { amount = n; }
+	else { amount = n + '.' + d; }
+	amount = minus + amount;
+	return amount;
+}
 
 <div suppressHydrationWarning={true}>
     { process.browser }
@@ -455,8 +477,9 @@ function galOverFebruaryFees() {
     <table className={styles.table}>
       <thead>
         <tr className={styles.th2}>
-          <th className={styles.th2}><h4><span>{data.last_name}</span><br /></h4></th>
-          <th className={styles.th2}><h4><span>#{data.meter_serialNum}</span></h4></th>
+        <td>
+          <th className={styles.th2}><h4><span>{data.last_name}</span><br /></h4></th></td>
+          <td><th className={styles.th2}><h4><span>#{data.meter_serialNum}</span></h4></th></td>
         </tr>
         <tr className={styles.th2}>
           <td>Billing Period</td>
