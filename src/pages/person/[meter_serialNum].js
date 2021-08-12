6a563e1d3fe2913c25f916af1ee06885af919174
ryan ernst <ryan@ryanernstnyberg.com>
2021-08-13 00:58:22 +0200

    asdf

--- src/pages/person/[meter_serialNum].js
+++ src/pages/person/[meter_serialNum].js
@@ -86,13 +86,13 @@ export default function person() {
             backgroundColor: bgColors.Blue}} 
               type="text" 
               id="info2" 
-              label="June 4th, 2021" 
+              label="August 4th, 2021" 
               value={data.aug04_21} 
               readOnly />
             </td>
         </tr>
         <tr>
-        <td className={styles.td3}><p className={styles.p}>Gallons used since June 4th 2021.</p></td>
+        <td className={styles.td3}><p className={styles.p}>Gallons used since August 4th 2021.</p></td>
         <td className={styles.td3}>
           <input style={{
             backgroundColor: bgColors.Blue}}
