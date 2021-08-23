aff0c47283afee089dbc5a603fa0d3d65b4f382a
ryan ernst <ryan@ryanernstnyberg.com>
2021-08-23 15:40:45 +0200

    Update [last_name].js

--- src/pages/api/overuse/[last_name].js
+++ src/pages/api/overuse/[last_name].js
@@ -1,7 +1,7 @@
-import { people } from '../../../../data'
+import { LastNames } from '../../../../data1'
 
 export default function overHandler({ query: { last_name } }, res) {
-  const filtered = people.filter((p) => p.last_name === last_name)
+  const filtered = LastNames.filter((p) => p.last_name === last_name)
 
   // User with meter_serialNum exists
   if (filtered.length > 0) {
