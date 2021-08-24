e4a3151a6aad129e32d4530512f7b527e7620bab
ryan ernst <ryan@ryanernstnyberg.com>
2021-08-25 00:32:06 +0200

    Update [meter_serialNum].js

--- src/pages/person/[meter_serialNum].js
+++ src/pages/person/[meter_serialNum].js
@@ -112,7 +112,7 @@ export default function person() {
             type="text" 
             name="utilized" 
             id="lastYear"
-            value={data.jun10_20-data.apr05_20} 
+            value={data. aug10_20-data.oct07_20} 
             label="answers" 
             readOnly>
           </input>
