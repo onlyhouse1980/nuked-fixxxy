d9c249237c65825e33a3f6d1a9684e485000aee6
ryan ernst <ryan@ryanernstnyberg.com>
2021-09-03 06:09:36 +0200

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,8 +3,6 @@ import styles from '../styles/Contact.module.css'
 import { motion } from 'framer-motion'
 import Image from 'next/image'
 import Link from 'next/link'
-import MailToButton from '../components/MailToButton'
-import { ButtonMailto } from '../components/ButtonMailto'
 
 const container = {
   hidden: { opacity: 0 },
