60d43321998f21e9eae3e6baf1d1678c30507127
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2021-10-11 21:35:34 +0200

    Update [last_name].js

--- src/pages/over/[last_name].js
+++ src/pages/over/[last_name].js
@@ -824,7 +824,7 @@ function galOverAugustFees21() {
 
   
     
- <main style={{width: '100vw', height: '100vh', width: '100%' , marginTop:'-80px', marginLeft:'--80px', paddingBottom: '50px'}}>
+ <main style={{width: '100vw', height: '100vh', width: '100%' , marginTop:'10px', marginLeft:'-80px', paddingBottom: '50px'}}>
   
       
         <Zoom top cascade>
