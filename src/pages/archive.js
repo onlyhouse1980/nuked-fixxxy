35c59e79f52060ad51859909a8f51a93a3d39ed3
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-01-09 09:51:50 +0100

    archiveup

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -37,51 +37,51 @@
         
         
         
-        <button><a target='_blank' href='/2014.pdf'>2014</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2013</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2012</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2011</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2010</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2009</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2008</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2007</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2014</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2013</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2012</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2011</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2010</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2009</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2008</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2007</a></button>
         
-        <button><a target='_blank' href='/2014.pdf'>2006</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2005</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2004</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2003</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2002</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2001</a></button>
-        <button><a target='_blank' href='/2014.pdf'>2000</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1999</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2006</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2005</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2004</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2003</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2002</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2001</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>2000</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1999</a></button>
         
-        <button><a target='_blank' href='/2014.pdf'>1998</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1997</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1996</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1995</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1994</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1993</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1992</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1991</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1998</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1997</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1996</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1995</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1994</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1993</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1992</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1991</a></button>
         
-        <button><a target='_blank' href='/2014.pdf'>1990</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1989</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1987</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1986</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1985</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1984</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1983</a></button>
-        <button><a target='_blank' href='/2014.pdf'>1982</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1990</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1989</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1987</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1986</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1985</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1984</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1983</a></button>
+        <button><a target='_blank' href='/archive/2014.pdf'>1982</a></button>
         
         
-        <button><a target='_blank' href='/2014.pdf'>1981</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1980</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1979</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1978</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1977</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1976</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1975</a></button>    
-        <button><a target='_blank' href='/2014.pdf'>1974</a></button>    
+        <button><a target='_blank' href='/archive/1981.pdf'>1981</a></button>    
+        <button><a target='_blank' href='/archive/1980.pdf'>1980</a></button>    
+        <button><a target='_blank' href='/archive/1979.pdf'>1979</a></button>    
+        <button><a target='_blank' href='/archive/1978.pdf'>1978</a></button>    
+        <button><a target='_blank' href='/archive/1977.pdf'>1977</a></button>    
+        <button><a target='_blank' href='/archive/1976.pdf'>1976</a></button>    
+        <button><a target='_blank' href='/archive/1975.pdf'>1975</a></button>    
+        <button><a target='_blank' href='/archive/1974.pdf'>1974</a></button>    
         </div>
         </div>
         
