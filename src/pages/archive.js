27efafa48ecd04fa39eed385917f6e4455c98119
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-01-11 18:28:07 +0100

    Update archive.js

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -37,43 +37,43 @@
         
         
         
-        <button className={styles.button}><a target='_blank' href='/archive/2013.pdf'>2013</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/2010.pdf'>2010</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/2008.pdf'>2008</a></button>
-       <button className={styles.button}><a target='_blank' href='/archive/20041016.pdf'>2004</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/2003.pdf'>2003</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/2002.pdf'>2002</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/2000.pdf'>2000</a></button>
+        <button className={styles.button}><a  href='/archive/2013.pdf'>2013</a></button>
+        <button className={styles.button}><a  href='/archive/2010.pdf'>2010</a></button>
+        <button className={styles.button}><a  href='/archive/2008.pdf'>2008</a></button>
+       <button className={styles.button}><a  href='/archive/20041016.pdf'>2004</a></button>
+        <button className={styles.button}><a  href='/archive/2003.pdf'>2003</a></button>
+        <button className={styles.button}><a  href='/archive/2002.pdf'>2002</a></button>
+        <button className={styles.button}><a  href='/archive/2000.pdf'>2000</a></button>
         
-        <button className={styles.button}><a target='_blank' href='/archive/1998.pdf'>1998</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1997.pdf'>1997</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1996.pdf'>1996</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1995.pdf'>1995</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1994.pdf'>1994</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1993.pdf'>1993</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1992.pdf'>1992</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1991.pdf'>1991</a></button>
+        <button className={styles.button}><a  href='/archive/1998.pdf'>1998</a></button>
+        <button className={styles.button}><a  href='/archive/1997.pdf'>1997</a></button>
+        <button className={styles.button}><a  href='/archive/1996.pdf'>1996</a></button>
+        <button className={styles.button}><a  href='/archive/1995.pdf'>1995</a></button>
+        <button className={styles.button}><a  href='/archive/1994.pdf'>1994</a></button>
+        <button className={styles.button}><a  href='/archive/1993.pdf'>1993</a></button>
+        <button className={styles.button}><a  href='/archive/1992.pdf'>1992</a></button>
+        <button className={styles.button}><a  href='/archive/1991.pdf'>1991</a></button>
         
-        <button className={styles.button}><a target='_blank' href='/archive/1990.pdf'>1990</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1989.pdf'>1989</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1988.pdf'>1988</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1987.pdf'>1987</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1986.pdf'>1986</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1985.pdf'>1985</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1984.pdf'>1984</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1983.pdf'>1983</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/19822.pdf'>19822</a></button>
-        <button className={styles.button}><a target='_blank' href='/archive/1982.pdf'>1982</a></button>
+        <button className={styles.button}><a  href='/archive/1990.pdf'>1990</a></button>
+        <button className={styles.button}><a  href='/archive/1989.pdf'>1989</a></button>
+        <button className={styles.button}><a  href='/archive/1988.pdf'>1988</a></button>
+        <button className={styles.button}><a  href='/archive/1987.pdf'>1987</a></button>
+        <button className={styles.button}><a  href='/archive/1986.pdf'>1986</a></button>
+        <button className={styles.button}><a  href='/archive/1985.pdf'>1985</a></button>
+        <button className={styles.button}><a  href='/archive/1984.pdf'>1984</a></button>
+        <button className={styles.button}><a  href='/archive/1983.pdf'>1983</a></button>
+        <button className={styles.button}><a  href='/archive/19822.pdf'>19822</a></button>
+        <button className={styles.button}><a  href='/archive/1982.pdf'>1982</a></button>
         
         
-        <button className={styles.button}><a target='_blank' href='/archive/1981.pdf'>1981</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1980.pdf'>1980</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1979.pdf'>1979</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1978.pdf'>1978</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1977.pdf'>1977</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1976.pdf'>1976</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1975.pdf'>1975</a></button>    
-        <button className={styles.button}><a target='_blank' href='/archive/1974.pdf'>1974</a></button>    
+        <button className={styles.button}><a  href='/archive/1981.pdf'>1981</a></button>    
+        <button className={styles.button}><a  href='/archive/1980.pdf'>1980</a></button>    
+        <button className={styles.button}><a  href='/archive/1979.pdf'>1979</a></button>    
+        <button className={styles.button}><a  href='/archive/1978.pdf'>1978</a></button>    
+        <button className={styles.button}><a  href='/archive/1977.pdf'>1977</a></button>    
+        <button className={styles.button}><a  href='/archive/1976.pdf'>1976</a></button>    
+        <button className={styles.button}><a  href='/archive/1975.pdf'>1975</a></button>    
+        <button className={styles.button}><a  href='/archive/1974.pdf'>1974</a></button>    
         </div>
         </div>
         
