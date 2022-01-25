18f0e7786e1f8a70eabc3eef96d3cacbf27439ca
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-01-25 11:10:58 +0100

    Update misc.js

--- src/pages/misc.js
+++ src/pages/misc.js
@@ -2,7 +2,6 @@ import VideoBg from '../components/VideoBg';
 import styles from '../styles/misc.module.css';
 import { motion } from 'framer-motion'
 
-        
         const container = {
           hidden: { opacity: 0 },
           show: {
@@ -12,38 +11,29 @@ import { motion } from 'framer-motion'
             }
           }
         }
-        
         const item = {
           hidden: { opacity: 0 },
           show: { opacity: 1 }
         }
-        
         const Misc = () => (
           <motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
-            <VideoBg /> 
+            <VideoBg />
             <motion.container className={styles.bgcontainer}
             variants={container}
             initial="hidden"
             animate="show" >
-            <h2 className='text-center'><b>OBCG</b><br /><b>Historical Documents</b></h2>
-            <p></p>
-            <div className={styles.divBlock}>
-            
-        
-        <div className={styles.indDates}>
-        
-        
-        
-        <button className={styles.button}><a  href='/misc/1982_Water_line_replacement.pdf'>1982 Water line replacement</a></button>
-        <button className={styles.button}><a  href='/misc/Legal_property.pdf'>Legal property</a></button>
-        <button className={styles.button}><a  href='/misc/Map_Master_Water_Line_Connections.pdf'>Map Water Line</a></button>
-        <button className={styles.button}><a  href='/misc/Park_Madings_Orchard_Beach.pdf'>Park Madings Orchard Beach </a></button>
-        <button className={styles.button}><a  href='/misc/assorted_documents.pdf'>Assorted Documents</a></button>
-        </div>
-        </div>
-        
-        </motion.container>
-        </motion.div>
+              <h2 className='text-center'><b>OBCG</b><br /><b>Historical Documents</b></h2>
+              <p></p>
+              <div className={styles.divBlock}>
+                <div className={styles.indDates}>
+                  <button className={styles.button}><a  href='/misc/1982_Water_line_replacement.pdf'>1982 Water line replacement</a></button>
+                  <button className={styles.button}><a  href='/misc/Legal_property.pdf'>Legal property</a></button>
+                  <button className={styles.button}><a  href='/misc/Map_Master_Water_Line_Connections.pdf'>Map Water Line</a></button>
+                  <button className={styles.button}><a  href='/misc/Park_Madings_Orchard_Beach.pdf'>Park Madings Orchard Beach </a></button>
+                  <button className={styles.button}><a  href='/misc/assorted_documents.pdf'>Assorted Documents</a></button>
+                </div>
+              </div>
+            </motion.container>
+          </motion.div>
         );
-        
         export default Misc;
