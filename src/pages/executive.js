76224b66f8b2b6765014729cde3533d1ff92ad75
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-04-04 14:59:39 +0200

    exec

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -28,13 +28,13 @@ const Executive = () => (
     <h2 className='text-center'><b>OBCG</b><br /><b>Executive Board</b></h2>
     <p></p>
     <div className={styles.divBlock}>
-      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-1.webp" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
-        <div className={styles.contentNameplate}><p>President<br />Jim Farrell<br /><a className={styles.clink}><Link href="/contactus1" passHref >Contact</Link></a></p></div>
+      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-1.webp" loading="lazy" width="300" alt="Member Photo President" className={styles.Image5} />
+        <div className={styles.contentNameplate}>  <p>President<br />Jim Farrell<br /><a className={styles.clink}><Link href="/contactus1" passHref >Contact</Link></a></p></div>
       </motion.div>
-      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-2.webp" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-2.webp" loading="lazy" width="300" alt="Member Vice President Photo Erika Aus" className={styles.Image5} />
         <div className={styles.contentNameplate}><p>Vice President<br />Erika Aust<br /><a className={styles.clink}><Link href="/contactus" passHref >Contact</Link></a></p></div>
       </motion.div>
-      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-3.webp" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+      <motion.div variants={item} className= {styles.content}><motion.img src="/Images/WebPFiles/team-3.webp" loading="lazy" width="300" alt="Member Photo Secretary/Treasurer James Loder" className={styles.Image5} />
         <div className={styles.contentNameplate}><p>Secretary/Treasurer<br />James Loder<br /><a className={styles.clink}><Link href="/contactus3" passHref >Contact</Link></a></p></div>
       </motion.div>
       </div>
