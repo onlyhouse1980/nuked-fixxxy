8492c2c2a636522f27212309c3261a2cdaf1b221
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-05-07 16:55:18 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@
       return [
         {
           source: '/erin',
-          destination: 'https://drive.google.com/file/d/10Y7Tm8f8d0csnq7EXWw6hForRgQroQ-D/view?ts=6272d6ba',
+          destination: 'https://drive.google.com/file/d/10Y7Tm8f8d0csnq7EXWw6hForRgQroQ-D/view?usp=sharing',
           permanent: true,
         },
       ]
