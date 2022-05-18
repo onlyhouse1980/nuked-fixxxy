5f3a60e16f70b5c7afca625048944ea071b6fc62
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-05-18 18:46:35 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -13,16 +13,16 @@ function Layout(props) {
     </Head>
     
       <Navbar style='z-index:10!important'/>
-      <MDBContainer>{props.children}</MDBContainer>
+      <MDBContainer style={{paddingTop:30}}>{props.children}</MDBContainer>
       
       <Footer />
       <style jsx global>{`
-        #__next {
+          endregion__next {
           max-width: 100vw;
           display: flex;
           flex-direction: column;
           justify-content: space-between;
-          width: 100%; 
+          width: 80%; 
           
           
 
