16533e18454990921a4d7cd1c80b80c942aa50bd
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-07-10 02:02:01 +0200

    cloudinary

---
+++ src/pages/cloudinary.js
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import Head from 'next/head'
+import styles from '../styles/Cloudinary.module.scss'
+
+export default function Cloudinary() {
+  const [imageSrc, setImageSrc] = useState();
+  const [uploadData, setUploadData] = useState();
+
+  /**
+   * handleOnChange
+   * @description Triggers when the file input changes (ex: when a file is selected)
+   */
+
+  function handleOnChange(changeEvent) {
+    const reader = new FileReader();
+
+    reader.onload = function(onLoadEvent) {
+      setImageSrc(onLoadEvent.target.result);
+      setUploadData(undefined);
+    }
+
+    reader.readAsDataURL(changeEvent.target.files[0]);
+  }
+
+  /**
+   * handleOnSubmit
+   * @description Triggers when the main form is submitted
+   */
+
+  async function handleOnSubmit(event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    const fileInput = 
+    Array.from(form.elements).find(({ name }) => name === 'file')
+    const formData = new FormData();
+
+    for( const file of fileInput.files ) {
+      formData.append('file', file);
+    }
+    
+    formData.append('upload_preset', 'unsignedUploads'); 
+
+const data = await fetch('https://api.cloudinary.com/v1_1/dfnaxhqqq/image/upload', {
+    method: 'POST',
+    body: formData
+}).then(r => r.json());
+
+  setImageSrc(data.secure_url);
+  setUploadData(data);
+
+console.log('data', data);
+
+  }
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Image Uploader</title>
+        <meta name="description" content="Upload your image to Cloudinary!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Image Uploader
+        </h1>
+
+        <p className={styles.description}>
+          Upload your image to OBCG!
+        </p>
+
+        <form className={styles.form} method="post" onChange={handleOnChange} onSubmit={handleOnSubmit}>
+          <p>
+            <input type="file" name="file" />
+          </p>
+          
+          <img src={imageSrc} />
+          
+          {imageSrc && !uploadData && (
+            <p>
+              <button>Upload Files</button>
+            </p>
+          )}
+
+          {uploadData && (
+            <code><pre>{JSON.stringify(uploadData, null, 2)}</pre></code>
+          )}
+        </form>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>Find the tutorial on <a href="https://spacejelly.dev/">spacejelly.dev</a>!</p>
+      </footer>
+    </div>
+  )
+}
