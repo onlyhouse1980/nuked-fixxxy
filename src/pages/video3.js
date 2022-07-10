65e695bb12f18504ee5d07246f924e350ab34266
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-07-11 01:50:18 +0200

    Create video3.js

---
+++ src/pages/video3.js
@@ -0,0 +1,25 @@
+// import VideoBg1 from '../components/VideoBg1'
+import styles from '../styles/About.module.css'
+import { motion } from 'framer-motion'
+import Zoom from 'react-reveal/Zoom'
+
+
+
+const Video2 = () => (
+<motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
+   
+    <Zoom top>
+    <motion.div className={styles.content}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0.2 }}>
+      <video className='fixed z-\[-1\]' controls muted loop style={{ width: '100%', height: '100%' }}
+         src="/whale3.mp4" />
+		 
+    </motion.div>
+    </Zoom>
+  </motion.div>
+
+);
+
+export default Video2;
