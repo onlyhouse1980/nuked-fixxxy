d9d10bc827579acd37b74e8ed5b8c554d86e83b1
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-07-12 16:31:07 +0200

    Create index.js

---
+++ src/pages/parades/index.js
@@ -0,0 +1,81 @@
+import styles from '../../styles/Whales.module.css'
+import Link from 'next/link'
+
+
+
+const fourthParades = () => {
+  
+    const media_urls = [
+      {
+        id: 1,
+        title: '2018 First Parade',
+        url: 'parades/parade1',
+        video_url:
+          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657276658/obcg/OBCG_2018_First_4th_of_July_Parade.mp4',
+      },
+      {
+        id: 2,
+        title: '2019 Parade',
+        url: 'parades/parade2',        
+        video_url:
+          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657276660/obcg/2019_4thOfJuly_Parade.mp4',
+      },
+      {
+        id: 3,
+        title: '2022 Parade',
+        url: 'parades/parade3',
+        video_url:
+          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657634987/obcg/OBCG_2022_16x9_qrxgj6.mp4',
+      },
+    ]
+  
+    // handle mouse enter
+    const handleMouseEnter = (e) => {
+      const vid = e.target
+      vid.muted = true
+      vid.play()
+    }
+  
+    // handle mouse leave
+    const handleMouseLeave = (e) => {
+      const vid = e.target
+      vid.muted = false
+      vid.currentTime = 0
+      vid.pause()
+    }
+  
+    return (
+      <>
+        <h1 className='text-center'>OBCG Annual 4th of July Parade</h1>
+        <div className='container'>
+          <div className='row'>
+            {media_urls.map((media) => (
+              <div key={media.id} className='col-lg-4 col-sm-6 mb-4'>
+                <Link href={media.url}>
+                <div className='card h-100 bg-indigo-500'>
+                  <div className={styles.cardBody}>
+                  <h4 className={styles.cardTitle}> {media.title}</h4>
+                    <video
+                      width='100%'
+                      height='10
+                      0%'
+                      padding-bottom='10px'
+                      controls
+                      onMouseEnter={handleMouseEnter}
+                      onMouseLeave={handleMouseLeave}
+                    >
+                      <source src={media.video_url} type='video/mp4' />
+                    </video>
+                  </div>
+                </div>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    )
+  }
+  
+  export default fourthParades
+  
