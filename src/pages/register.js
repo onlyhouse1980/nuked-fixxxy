db10a9dc044986dc4c45bd0792751278ab79d40c
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-10-30 08:18:27 +0100

    Update register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -15,7 +15,7 @@ import React from 'react'
 import { MDBAnimation } from "mdbreact";
 /* import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content' */
-import { RmoveLabel } from '../components/removeLabel'
+
 import useSWR from 'swr'
 
 
