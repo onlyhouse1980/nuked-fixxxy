e94980ef247da729922a6c3a8b82f86bba144b84
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-23 23:41:20 +0200

    pdfs
    touchable ios pdfs

---
+++ src/pages/pdfs/consumer.js
@@ -0,0 +1,7 @@
+export default function Consumer() {
+    return (
+    <div style={{position:"relative", height:"100vh", width:"100%"}}>
+        <iframe src="https://drive.google.com/file/d/14AbNqAL7drm7Ucyzz1ReiNst_E6Jy5Qo/preview" width="100%" height="900px"></iframe>  
+    </div>
+    )
+}
