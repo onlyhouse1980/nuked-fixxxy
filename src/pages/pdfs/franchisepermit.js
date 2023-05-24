bdb9204575e54d6bb42bc1da08faf8968742fc46
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-24 05:44:58 +0200

    updated next react and added franchise.pdf

---
+++ src/pages/pdfs/franchisepermit.js
@@ -0,0 +1,18 @@
+
+
+
+export default function Franchise() {
+    return (
+     
+      
+  <div style={{position:"relative", height:"100vh", width:"100%"}}> 
+  
+    
+  
+    <iframe src="https://drive.google.com/file/d/19seZvVdTG5O5Q0H1qdrh_F6MG1ZcLfto/preview"  width="100%" height="900px"></iframe>
+      
+    </div>
+    
+  
+    );
+  }
