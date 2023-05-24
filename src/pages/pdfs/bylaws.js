46bd836ba0a8cd2d5849def065722be1c08d9008
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-24 04:53:55 +0200

    pdfs

---
+++ src/pages/pdfs/bylaws.js
@@ -0,0 +1,9 @@
+
+
+export default function Bylaws() {
+    return (
+    <div style={{position:"relative", height:"100vh", width:"100%"}}>
+        <iframe src="https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview" width="100%" height="900px"></iframe>  
+    </div>
+    )
+}
---
+++ src/pages/pdfs/proposed.js
@@ -0,0 +1,9 @@
+
+
+export default function ProposedBylaws() {
+    return (
+    <div style={{position:"relative", height:"100vh", width:"100%"}}>
+        <iframe src="https://drive.google.com/file/d/13TcgI9BiJH3Z8528XMSJ51e7elT5v6d9/preview" width="100%" height="900px"></iframe>  
+    </div>
+    )
+}
