4367b1539a07d260ded5e00c9c4c3488a3e96c5c
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-26 13:12:08 +0200

    Update efficiency.js

--- src/pages/pdfs/efficiency.js
+++ src/pages/pdfs/efficiency.js
@@ -3,7 +3,7 @@ export const App = () =>  {
   const [embedURL]=useState('https://drive.google.com/file/d/1LkpS55BOe5YzoeHwnCNpe8cNYlM3jWsh/preview')
   return (
    <div className='container'>
-    <h1>Water Use Efficiency</h1>
+    <h5>Water Use Efficiency</h5>
     <iframe src={embedURL} width="100%" height="500"></iframe>
   </div>
 
