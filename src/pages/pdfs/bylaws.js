4aa6ed4ba8f28e697f14e89e31112c3addcdd69a
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-26 13:00:43 +0200

    pdfs
    bylaws and faq

--- src/pages/pdfs/bylaws.js
+++ src/pages/pdfs/bylaws.js
@@ -3,7 +3,7 @@ export const App = () =>  {
   const [embedURL]=useState('https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview')
   return (
    <div className='container'>
-    <h1>Water Use Efficiency</h1>
+    <h5>Water Use Efficiency</h5>
     <iframe src={embedURL} width="100%" height="480"></iframe>
   </div>
 
