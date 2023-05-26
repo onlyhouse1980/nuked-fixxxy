a343340ac8c9aefac370dfd0ca24603384a4ab0c
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-26 13:08:25 +0200

    Update proposed.js

--- src/pages/pdfs/proposed.js
+++ src/pages/pdfs/proposed.js
@@ -3,7 +3,7 @@
 export default function ProposedBylaws() {
     return (
     <div style={{position:"relative", height:"100vh", width:"100%"}}>
-        <iframe src="https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview" width="100%" height="900px"></iframe>  
+        <iframe src="https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview" width="100%" height={700}></iframe>  
     </div>
     )
 }
