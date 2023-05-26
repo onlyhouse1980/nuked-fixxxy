e8dfff2bb85a283641c8ce67ed2ea37d559ce8e7
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-05-26 12:36:06 +0200

    pdf test
    bylaws efficiency

--- src/pages/pdfs/bylaws.js
+++ src/pages/pdfs/bylaws.js
@@ -1,9 +1,12 @@
+import React, { useState } from 'react';
+export const App = () =>  {
+  const [embedURL]=useState('https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview')
+  return (
+   <div className='container'>
+    <h1>Water Use Efficiency</h1>
+    <iframe src={embedURL} width="640" height="480"></iframe>
+  </div>
 
-
-export default function Bylaws() {
-    return (
-    <div style={{position:"relative", height:"100vh", width:"100%"}}>
-        <iframe src="https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview" width="100%" height="900px"></iframe>  
-    </div>
-    )
+  )
 }
+  export default App;
--- src/pages/pdfs/efficiency.js
+++ src/pages/pdfs/efficiency.js
@@ -1,16 +1,13 @@
-
-export default function Efficiency() {
+import React, { useState } from 'react';
+export const App = () =>  {
+  const [embedURL]=useState('https://drive.google.com/file/d/1LkpS55BOe5YzoeHwnCNpe8cNYlM3jWsh/preview')
   return (
-   
-    
-<div style={{position:"relative", height:"100vh", width:"100%"}}> 
-
-  
-
-  <iframe src="https://drive.google.com/file/d/1LkpS55BOe5YzoeHwnCNpe8cNYlM3jWsh/preview"  width="100%" height="900px"></iframe>
-    
+   <div className='container'>
+    <h1>Water Use Efficiency</h1>
+    <iframe src={embedURL} width="640" height="480"></iframe>
   </div>
-  
 
   );
 }
+
+export default App;
