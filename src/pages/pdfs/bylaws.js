b037c652de1e8df1ea7b11235a3282be2bc0eaa8
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-08 17:50:06 +0200

    little stuff

--- src/pages/pdfs/bylaws.js
+++ src/pages/pdfs/bylaws.js
@@ -1,14 +1,13 @@
+import React, { useState } from 'react'
 
-  import React, { useState } from 'react';
 export const App = () =>  {
   const [embedURL]=useState('https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview')
   return (
-   <div className='container'>
-    <h5>Current Bylaws</h5>
-    <iframe src={embedURL} width="100%" height="500"></iframe>
-  </div>
-
-  );
+    <div className='container'>
+      <h5>Current Bylaws</h5>
+      <iframe src={embedURL} width="100%" height="500"></iframe>
+    </div>
+  )
 }
 
-export default App;
+export default App
