f54da80d92f87d69d7c91a068991caf3a296af94
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-10 01:15:20 +0200

    archive folder
    added incomplete archive folder

---
+++ src/pages/pdfs/misc/minutes/archive/06_21_2014Minutes.js
@@ -0,0 +1,7 @@
+export default function Minutes20142106() {
+    return (
+    <div style={{position:"relative", height:"900px", width:"100%"}}>
+        <iframe src="https://drive.google.com/file/d/1zJRynuWXjQY63jcr04hz_47jMvG_0JBL/preview" width="640" height="480" allow="autoplay"></iframe>
+    </div>
+    )
+}
---
+++ src/pages/pdfs/misc/minutes/archive/2006.js
@@ -0,0 +1,8 @@
+
+export default function Minutes2006() {
+    return (
+    <div style={{position:"relative", height:"900px", width:"100%"}}>
+       <iframe src="https://drive.google.com/file/d/1nwDXFmV29hwZ_DozNy4Yw4yEf_m4qzXN/preview" width="640" height="480" allow="autoplay"></iframe>  
+    </div>
+    )
+}
