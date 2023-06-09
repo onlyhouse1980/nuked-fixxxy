a713758f5e268efc73250555d7e997e2118f5fe2
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-10 00:39:06 +0200

    updated the pdfs page for 2015Minutes

--- src/pages/pdfs/misc/minutes/2015Minutes.js
+++ src/pages/pdfs/misc/minutes/2015Minutes.js
@@ -1,7 +1,7 @@
 export default function Minutes2015() {
     return (
     <div style={{position:"relative", height:"100vh", width:"100%"}}>
-        <iframe src="https://drive.google.com/file/d/1vYiycCKi8JxqVdCYdAhhMTu9d1dqyiLd/preview" width="100%" height="900px"></iframe>  
+        <iframe src="https://drive.google.com/file/d/1X_T02TuaGcFWZDSD1Q2VIPre-_6AuXJ_/preview" width="100%" height="900px" allow="autoplay"></iframe>  
     </div>
     )
 }
