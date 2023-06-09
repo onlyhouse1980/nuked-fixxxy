8af2661015c36bf0ec4bbde3ac0a1dd68826b9f7
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-09 14:21:00 +0200

    Add files via upload

--- src/pages/parades/parade1.js
+++ src/pages/parades/parade1.js
@@ -1,37 +1,25 @@
-'use client'
-
+import { CldVideoPlayer } from 'next-cloudinary';
 import styles from '../../styles/About.module.css'
-import { motion } from 'framer-motion'
-import Zoom from 'react-reveal/Zoom'
-import ReactPlayer from 'react-player';
-import React from 'react';
+
+export default function Parade1() {
+  return (
+<CldVideoPlayer 
+    id='obcg_zigy1j'
+    width='1920'
+    height='1080'
+    src="https://res.cloudinary.com/dfnaxhqqq/video/upload/fl_any_format.mono/v1657634720/obcg/OBCG_2018__4th_July_Parade_p8kgjw.mp4" 
+    imageUrl="https://obcg.org/Images/obcglogo.webp"
+    controls
+    autoPlay={false}
+    loop
+    muted
+    colors={{
+      base: '#3f51b5',
+      text: 'white',
+      accent: '#778899',
+  }}
+/>
 
 
-const Parade1 = () => (
-<motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
-   
-    <Zoom top>
-    <motion.div className={styles.content}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.2 }}>
-      
-		 <ReactPlayer
-      config={{ file: { attributes: { controlsList: 'nodownload' } } }}
-      onContextMenu={e => e.preventDefault()}
-      url="https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657634720/obcg/OBCG_2018__4th_July_Parade_p8kgjw.mp4#t=0.001" 
-      className='react-player'
-      playing={true} 
-      loop={true} 
-      muted={true} 
-      controls
-      width='100%' 
-      height='100%' />
-      
-    </motion.div>
-    </Zoom>
-  </motion.div>
-
-);
-
-export default Parade1;
+  )
+}
