ee3fe3df6b27a974eb883e7da2664519408a5da8
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-10 01:24:05 +0200

    Add files via upload

--- src/pages/pdfs/misc/minutes/archive/archive.js
+++ src/pages/pdfs/misc/minutes/archive/archive.js
@@ -1,4 +1,4 @@
-import styles from '../../../../../archive.module.css';
+import styles from '../../../../styles/Home.module.css';
 import { motion } from 'framer-motion'
 
 export async function getStaticProps() {
