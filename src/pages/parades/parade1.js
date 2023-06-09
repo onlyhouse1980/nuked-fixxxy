58fb899c1f65db5bfdc9346ad106ed5076c94d75
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-09 09:49:47 +0200

    parade1.js react-player

--- src/pages/parades/parade1.js
+++ src/pages/parades/parade1.js
@@ -1,7 +1,10 @@
+'use client'
+
 import styles from '../../styles/About.module.css'
 import { motion } from 'framer-motion'
 import Zoom from 'react-reveal/Zoom'
-
+import ReactPlayer from 'react-player';
+import React from 'react';
 
 
 const Parade1 = () => (
@@ -12,9 +15,19 @@ const Parade1 = () => (
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.2 }}>
-      <video className='fixed z-\[-1\]' controls muted loop style={{ width: '100%', height: '100%' }}
-         src="https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657634720/obcg/OBCG_2018__4th_July_Parade_p8kgjw.mp4#t=0.001" />
-		 
+      
+		 <ReactPlayer
+      config={{ file: { attributes: { controlsList: 'nodownload' } } }}
+      onContextMenu={e => e.preventDefault()}
+      url="https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657634720/obcg/OBCG_2018__4th_July_Parade_p8kgjw.mp4#t=0.001" 
+      className='react-player'
+      playing={true} 
+      loop={true} 
+      muted={true} 
+      controls
+      width='100%' 
+      height='100%' />
+      
     </motion.div>
     </Zoom>
   </motion.div>
