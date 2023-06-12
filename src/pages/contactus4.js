e88ba42435525de6b17169916ec24585072c0d5c
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-13 00:44:28 +0200

    Add files via upload

---
+++ src/pages/contactus4.js
@@ -0,0 +1,11 @@
+import Contact from "../components/pattern/contacttest";
+
+const Contactus4Page = () => {
+  return (
+    <div>
+      <Contact />
+    </div>
+  );
+};
+
+export default Contactus4Page;
