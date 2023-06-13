dc0165c45c61f32e6e30d102d58f552857171a0f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-13 19:02:55 +0200

    responsive 2023Minutes

--- src/pages/pdfs/misc/minutes/2023Minutes.js
+++ src/pages/pdfs/misc/minutes/2023Minutes.js
@@ -1,11 +1,18 @@
 import React, { useState } from "react";
+import styles from "./2023Minutes.module.css";
+
 export const App = () => {
   const [embedURL] = useState(
     "https://drive.google.com/file/d/1c3pBwyOy7yxEkmbGmFFs0JJ0-Pt-CwwF/preview"
   );
   return (
-    <div className="container">
-      <iframe src={embedURL} width="100%" height="500"></iframe>
+    <div className={styles.container}>
+      <iframe
+        className={styles.iframe}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
     </div>
   );
 };
