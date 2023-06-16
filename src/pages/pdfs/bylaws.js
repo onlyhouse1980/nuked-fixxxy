ad0a4354cba049570c712f49a6e35497e50e935c
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-17 00:41:25 +0200

    Add files via upload

--- src/pages/pdfs/bylaws.js
+++ src/pages/pdfs/bylaws.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import styles from "../../styles/pdfs.module.css";
 
-export const App = () =>  {
-  const [embedURL]=useState('https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview')
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview"
+  );
   return (
-    <div className='container'>
-      <h5>Current Bylaws</h5>
-      <iframe src={embedURL} width="100%" height="500"></iframe>
+    <div className="container">
+      <h5>Past Bylaws</h5>
+      <iframe
+        className={styles.iframPDF}
+        src={embedURL}
+        width="100%"
+        height="1000px"
+      ></iframe>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/pages/pdfs/proposed.js
+++ src/pages/pdfs/proposed.js
@@ -1,15 +1,21 @@
+import React, { useState } from "react";
+import styles from "../../styles/pdfs.module.css";
 
-
-import React, { useState } from 'react';
-export const App = () =>  {
-  const [embedURL]=useState('https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview')
-  return (
-   <div className='container'>
-    
-    <iframe src={embedURL} width="100%" height="500"></iframe>
-  </div>
-
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview"
   );
-}
+  return (
+    <div className="container">
+      <h5>Current Bylaws</h5>
+      <iframe
+        className={styles.iframPDF}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
+    </div>
+  );
+};
 
 export default App;
