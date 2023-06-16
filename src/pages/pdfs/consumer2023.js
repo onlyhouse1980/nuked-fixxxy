ecf94b568a575cf8e5669af684b145ee3745c0a0
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-16 23:33:03 +0200

    Add files via upload

---
+++ src/pages/pdfs/consumer2023.js
@@ -0,0 +1,20 @@
+import styles from "pdfs.module.css";
+import React, { useState } from "react";
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1C5qGn2uNg4T6pxUdgwVobAlTngwSJa-N/preview"
+  );
+  return (
+    <div className="container">
+      <h5>Consumer Confidencce 2022</h5>
+      <iframe
+        className={styles.iframe}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
