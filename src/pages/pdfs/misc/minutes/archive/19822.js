093e84a213eb791e171add09c471199bbc59cac4
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-17 07:50:56 +0200

    Add files via upload

--- src/pages/pdfs/misc/minutes/archive/19822.js
+++ src/pages/pdfs/misc/minutes/archive/19822.js
@@ -1,4 +1,6 @@
-https: import React, { useState } from "react";
+import React, { useState } from "react";
+import styles from "../../../../../styles/pdfs.module.css";
+
 export const App = () => {
   const [embedURL] = useState(
     "https://drive.google.com/file/d/1F9I5NOzNpk1Zn1rdw8LWzA9691b5PJaC/preview"
@@ -7,7 +9,7 @@ export const App = () => {
     <div className="container">
       <h5>1982 Annual Meeting Minutes</h5>
       <iframe
-        className={styles.iframPDF}
+        className={styles.iframPDFBL}
         src={embedURL}
         width="100%"
         height="530"
