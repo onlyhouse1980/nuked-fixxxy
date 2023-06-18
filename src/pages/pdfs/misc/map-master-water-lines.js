1e92c459212be3da14ceb3c351f387cb6b08cd0f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-18 03:39:12 +0200

    map-master-waterlines....

--- src/pages/pdfs/misc/map-master-water-lines.js
+++ src/pages/pdfs/misc/map-master-water-lines.js
@@ -1,12 +1,19 @@
+import styles from "../../../styles/pdfs.module.css";
 import React, { useState } from "react";
+
 export const App = () => {
   const [embedURL] = useState(
-    "https://drive.google.com/file/d/1wMxlrlq1APB6GuutUMPM1XdjhfEkPxeK/preview"
+    "https://drive.google.com/file/d/1a9PIyIOsNl5-cauzBE-bNGQMlTaaFZPR/preview"
   );
   return (
     <div className="container">
       <h5>Map Master Water Lines</h5>
-      <iframe src={embedURL} width="100%" height="500"></iframe>
+      <iframe
+        className={styles.iframPDFBL}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
     </div>
   );
 };
