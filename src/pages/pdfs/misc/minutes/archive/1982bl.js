8884d493d074e938fbb188d71c9e271d3f0b200b
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-06-18 02:44:44 +0200

    the 80s minutes

--- src/pages/pdfs/misc/minutes/archive/1982bl.js
+++ src/pages/pdfs/misc/minutes/archive/1982bl.js
@@ -1,18 +1,18 @@
-import styles from "../../../../../styles/pdfs.module.css";
 import React, { useState } from "react";
+import styles from "../../../../../styles/pdfs.module.css";
 
 export const App = () => {
   const [embedURL] = useState(
-    "https://drive.google.com/file/d/1ECVM9j3E-v3mfQxkC9TKWpWYga68ekvI/preview"
+    "https://drive.google.com/file/d/1GetWbBe20xPjJNGj_FJyAWFSuLZLXLqP/preview"
   );
   return (
     <div className="container">
-      <h5>1982 Special Meeting</h5>
+      <h5>1982 Bilaws</h5>
       <iframe
         className={styles.iframPDFBL}
         src={embedURL}
         width="100%"
-        height="530"
+        height="500"
       ></iframe>
     </div>
   );
--- src/pages/pdfs/misc/minutes/archive/1984.js
+++ src/pages/pdfs/misc/minutes/archive/1984.js
@@ -1,15 +1,14 @@
-import styles from "../../../../../styles/pdfs.module.css";
 import React, { useState } from "react";
+import styles from "../../../../../styles/pdfs.module.css";
 
 export const App = () => {
   const [embedURL] = useState(
-    "https://drive.google.com/file/d/1ECVM9j3E-v3mfQxkC9TKWpWYga68ekvI/preview"
+    "https://drive.google.com/file/d/1kha2HzX4_nQdHCayBHu8uuP3G1GAJR1y/preview"
   );
   return (
     <div className="container">
-      <h5>1982 Special Meeting</h5>
       <iframe
-        className={styles.iframPDFBL}
+        className={styles.iframPDF}
         src={embedURL}
         width="100%"
         height="530"
