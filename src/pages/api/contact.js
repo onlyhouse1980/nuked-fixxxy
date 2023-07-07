b9f308d74338f8b16707fd1fa0a199e46e891aee
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-07-07 18:50:32 +0200

    Add files via upload

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,50 +1,48 @@
-import nodemailer from "nodemailer"
-import { ContextReplacementPlugin } from "webpack"
+import { mailOptions, transporter } from "../../config/nodemailer";
 
-const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    auth: {
-        user: process.env.USERNAME,
-        pass: process.env.PASSWORD
+const CONTACT_MESSAGE_FIELDS = {
+  name: "Name",
+  email: "Email",
+  subject: "Subject",
+  message: "Message",
+};
+
+const generateEmailContent = (data) => {
+  const stringData = Object.entries(data).reduce(
+    (str, [key, val]) =>
+      (str += `${CONTACT_MESSAGE_FIELDS[key]}: \n${val} \n \n`),
+    ""
+  );
+  const htmlData = Object.entries(data).reduce((str, [key, val]) => {
+    return (str += `<h3 class="form-heading" align="left">${CONTACT_MESSAGE_FIELDS[key]}</h3><p class="form-answer" align="left">${val}</p>`);
+  }, "");
+
+  return {
+    text: stringData,
+    html: `<!DOCTYPE html><html> <head> <title></title> <meta charset="utf-8"/> <meta name="viewport" content="width=device-width, initial-scale=1"/> <meta http-equiv="X-UA-Compatible" content="IE=edge"/> <style type="text/css"> body, table, td, a{-webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;}table{border-collapse: collapse !important;}body{height: 100% !important; margin: 0 !important; padding: 0 !important; width: 100% !important;}@media screen and (max-width: 525px){.wrapper{width: 100% !important; max-width: 100% !important;}.responsive-table{width: 100% !important;}.padding{padding: 10px 5% 15px 5% !important;}.section-padding{padding: 0 15px 50px 15px !important;}}.form-container{margin-bottom: 24px; padding: 20px; border: 1px dashed #ccc;}.form-heading{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 400; text-align: left; line-height: 20px; font-size: 18px; margin: 0 0 8px; padding: 0;}.form-answer{color: #2a2a2a; font-family: "Helvetica Neue", "Helvetica", "Arial", sans-serif; font-weight: 300; text-align: left; line-height: 20px; font-size: 16px; margin: 0 0 24px; padding: 0;}div[style*="margin: 16px 0;"]{margin: 0 !important;}</style> </head> <body style="margin: 0 !important; padding: 0 !important; background: #fff"> <div style=" display: none; font-size: 1px; color: #fefefe; line-height: 1px;  max-height: 0px; max-width: 0px; opacity: 0; overflow: hidden; " ></div><table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td bgcolor="#ffffff" align="center" style="padding: 10px 15px 30px 15px" class="section-padding" > <table border="0" cellpadding="0" cellspacing="0" width="100%" style="max-width: 500px" class="responsive-table" > <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0"> <tr> <td> <table width="100%" border="0" cellspacing="0" cellpadding="0" > <tr> <td style=" padding: 0 0 0 0; font-size: 16px; line-height: 25px; color: #232323; " class="padding message-content" > <h2>New Contact Message</h2> <div class="form-container">${htmlData}</div></td></tr></table> </td></tr></table> </td></tr></table> </td></tr></table> </body></html>`,
+  };
+};
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+    if (!data || !data.name || !data.email || !data.subject || !data.message) {
+      return res.status(400).send({ message: "Bad request" });
     }
-})
-//[1]
 
-export default Contact().then(() => { 
+    try {
+      await transporter.sendMail({
+        ...mailOptions,
+        ...generateEmailContent(data),
+        subject: data.subject,
+      });
 
-async (req, res) => {
-    const { senderMail, name, tel, content, recipientMail } = req.body
-//[2]
-
-    // Check if fields are all filled
-    if (senderMail === "" || name === "" || content === "" || tel === "" || recipientMail === "") {
-        res.status(403).send("")
-        return
+      return res.status(200).json({ success: true });
+    } catch (err) {
+      console.log(err);
+      return res.status(400).json({ message: err.message });
     }
-//[3]
-
-    const mailerRes = await mailer({ senderMail, name, text: content + " ( phone )" + tel, recipientMail })
-    res.send(mailerRes)
-//[4]
-}
-
-const mailer = ({ senderMail, name, text, recipientMail }) => {
-    const from = name && senderMail ? `${name} <${senderMail}>` : `${name || senderMail}`
-    const message = {
-        from,
-        to: `${recipientMail}`,
-        subject: `New message from ${from}`,
-        text,
-        replyTo: from
-    }
-//[5]
-
-    return new Promise((resolve, reject) => {
-        transporter.sendMail(message, (error, info) =>
-            error ? reject(error) : resolve(info)
-        )
-    })
-//[6]
-}}
-)
+  }
+  return res.status(400).json({ message: "Bad request" });
+};
+export default handler;
