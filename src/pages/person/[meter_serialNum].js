ef377221db802fb4072ffdffd7ff26e04845c4be
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-07-15 23:04:25 +0200

    updated to june new year

--- src/pages/person/[meter_serialNum].js
+++ src/pages/person/[meter_serialNum].js
@@ -1,201 +1,246 @@
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-import styles from '../person/[meter_serialNum].module.css'
-import Zoom from 'react-reveal/Zoom';
-import React, { useState } from 'react';
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import styles from "../person/[meter_serialNum].module.css";
+import Zoom from "react-reveal/Zoom";
+import React, { useState } from "react";
 
-import { MDBBtn } from 'mdbreact';
+import { MDBBtn } from "mdbreact";
 
-<div suppressHydrationWarning={true}>
-    { process.browser }
-    </div>
-var bgColors = { "Default": "#81b71a",
-"Blue": "#00B1E1",
-"Cyan": "#37BC9B",
-"Green": "#8CC152",
-"Red": "#E9573F",
-"Yellow": "#F6BB42",
+<div suppressHydrationWarning={true}>{process.browser}</div>;
+var bgColors = {
+  Default: "#81b71a",
+  Blue: "#00B1E1",
+  Cyan: "#37BC9B",
+  Green: "#8CC152",
+  Red: "#E9573F",
+  Yellow: "#F6BB42",
 };
 const fetcher = async (url) => {
-  const res = await fetch(url)
-  const data = await res.json()
+  const res = await fetch(url);
+  const data = await res.json();
 
   if (res.status !== 200) {
-    throw new Error(data.message)
+    throw new Error(data.message);
   }
-  return data 
-}
+  return data;
+};
 
 export default function Person() {
-  
-  const { query } = useRouter()
+  const { query } = useRouter();
   const { data, error } = useSWR(
     () => query.meter_serialNum && `/api/people/${query.meter_serialNum}`,
     fetcher
-  )
-  if (error) return <div><p>Serial number not found. Please review <a href="/howtoreadmeter.pdf"><b>How to read your meter</b></a></p></div>
-  if (!data) return <div>Loading...</div>
+  );
+  if (error)
+    return (
+      <div>
+        <p>
+          Serial number not found. Please review{" "}
+          <a href="/howtoreadmeter.pdf">
+            <b>How to read your meter</b>
+          </a>
+        </p>
+      </div>
+    );
+  if (!data) return <div>Loading...</div>;
 
-    function sayHi() {
-      
-      let info1 = parseInt(document.getElementById("info1").value);
-      let info2 = parseInt(document.getElementById("info2").value);
-      var Answer = document.getElementById("Answer");
-        Answer.value = info1 - info2;
-      }
-    
-      function usedSofar(props) {
-        let info1 = parseInt(document.getElementById("info1").value);
-        let info2 = parseInt(document.getElementById("info2").value);
-        var Answer = document.getElementById("thisYear");
-          Answer.value =  Number(info1 - info2) + Number(Answer.value) ;
-        }
+  function sayHi() {
+    let info1 = parseInt(document.getElementById("info1").value);
+    let info2 = parseInt(document.getElementById("info2").value);
+    var Answer = document.getElementById("Answer");
+    Answer.value = info1 - info2;
+  }
 
-    function moreInfo(props) {
-      let info1 = parseInt(document.getElementById("info1").value);
-      let info2 = parseInt(document.getElementById("info2").value);
-      let info3 = parseInt(document.getElementById("thisYear").value);
-      var Answer = document.getElementById("Percent");
+  function usedSofar(props) {
+    let info1 = parseInt(document.getElementById("info1").value);
+    let info2 = parseInt(document.getElementById("info2").value);
+    var Answer = document.getElementById("thisYear");
+    Answer.value = Number(info1 - info2) + Number(Answer.value);
+  }
 
-        Answer.value =  (((info1 - info2 + info3) / 48000 )*100).toFixed(0) + "%";
-      }
-        
-    
-  return ( 
-    
-  <>
-    <Zoom top cascade>
-    
-    <table className={styles.table}>
-      <thead>
-        <tr>
-          <th className={styles.th2}><h4><span> Meter Serial # : {data.meter_serialNum}</span><br /></h4></th>
-          <th className={styles.th2}><h4><span>{data.last_name} </span></h4></th>
-        </tr>
-        
-      </thead>
-      <tbody>
-        
-        <tr>
-          <td className={styles.th}><p className={styles.p}>Enter reading from your meter in the box to the right.<br />(digits only - no, seperator eg. 1536987)<br /><br /> Then click the calculate button below that.*</p></td>
-          <td className={styles.thInput}><input style={{
-            backgroundColor: bgColors.Cyan }} type="text" id="info1" label="your reading" placeholder="Enter Your Reading" name="uInput"></input><br /><MDBBtn gradient="blue" onClick={() => {
-            sayHi()
-            usedSofar()
-            moreInfo()
-          }}
-          
-                 >Calculate</MDBBtn></td>
-        </tr>
-        <tr>
-          <td className={styles.td3}>
-          <p className={styles.p}></p>
-          <p className={styles.p}>Last reading by Vendor - December 9th, 2022</p>
-          </td>
-          <td className={styles.td3}>
-          <input style={{
-            backgroundColor: bgColors.Blue}} 
-              type="text" 
-              id="info2" 
-              label="December 9th, 2022" 
-              value={data.dec09_22} 
-              readOnly />
-            </td>
-        </tr>
-        <tr>
-        <td className={styles.td3}><p className={styles.p}>Gallons used since December 9th 2022.</p></td>
-        <td className={styles.td3}>
-          <input style={{
-            backgroundColor: bgColors.Blue}}
-            type="text" 
-            name="utilized" 
-            id="Answer" 
-            label="answers" 
-            readOnly>
-          </input>
-        </td>
-        </tr>
-        <tr>
-          <td className={styles.td3}><p className={styles.p}>Total gallons used this annual billing period (of 48,000)</p></td>
-          <td className={styles.td3}>
-          <input style={{
-            backgroundColor: bgColors.Blue}}
-            type="text" 
-            name="utilized" 
-            id="thisYear"
-            value={data.year_total} 
-            label="answers" 
-            readOnly>
-          </input>
-          </td>
-        </tr>
-        
-        <tr>
-          <td className={styles.td3} function person>
-          
-          <p className={styles.p}>Percentage annual allotment (48,000 gallons) used.<br /></p>
-          </td>  
-          <td className={styles.td3}>
-            
-            
-          <input style={{
-            backgroundColor: bgColors.Blue}} type="text" id="Percent" label="answers"  readOnly
-            >
-              
-              </input> 
-          
-                  
-                 
-                 
-          </td>
-        </tr>
+  function moreInfo(props) {
+    let info1 = parseInt(document.getElementById("info1").value);
+    let info2 = parseInt(document.getElementById("info2").value);
+    let info3 = 48000;
+    var Answer = document.getElementById("Percent");
 
-        <tr>
-          <td className={styles.td3} function person>
-          
-          <p className={styles.p}>End of billing year.<br /></p>
-          </td>  
-          <td className={styles.td3}>
-            
-            
-          <input style={{
-            backgroundColor: bgColors.Blue}} type="text" id="yearEnd1" value={data.billing_year_end} label="answers"  readOnly
-            >
-              
-              </input> 
-          
-                  
-                 
-                 
-          </td>
-        </tr>
+    Answer.value = (((info1 - info2) / info3) * 100).toFixed(0) + "%";
+  }
 
-        <tr>
-          <td className={styles.h5head}>
-          <h5>*If you are having problems with the<br />form, we recommend reading &quot;<a href="/howtoreadmeter.pdf">How to read your meter?</a>&quot; </h5>
-          
-          </td>
-          <td className={styles.td0}>
-          
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    
+  return (
+    <>
+      <Zoom top cascade>
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.th2}>
+                <h4>
+                  <span> Meter Serial # : {data.meter_serialNum}</span>
+                  <br />
+                </h4>
+              </th>
+              <th className={styles.th2}>
+                <h4>
+                  <span>{data.last_name} </span>
+                </h4>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className={styles.th}>
+                <p className={styles.p}>
+                  Enter reading from your meter in the box to the right.
+                  <br />
+                  (digits only - no, seperator eg. 1536987)
+                  <br />
+                  <br /> Then click the calculate button below that.*
+                </p>
+              </td>
+              <td className={styles.thInput}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Cyan,
+                  }}
+                  type="text"
+                  id="info1"
+                  label="your reading"
+                  placeholder="Enter Your Reading"
+                  name="uInput"
+                ></input>
+                <br />
+                <MDBBtn
+                  gradient="blue"
+                  onClick={() => {
+                    sayHi();
+                    usedSofar();
+                    moreInfo();
+                  }}
+                >
+                  Calculate
+                </MDBBtn>
+              </td>
+            </tr>
+            <tr>
+              <td className={styles.td3}>
+                <p className={styles.p}></p>
+                <p className={styles.p}>Last reading by Vendor - June 2023</p>
+              </td>
+              <td className={styles.td3}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Blue,
+                  }}
+                  type="text"
+                  id="info2"
+                  label="December 9th, 2022"
+                  value={data.jun05_23}
+                  readOnly
+                />
+              </td>
+            </tr>
+            <tr>
+              <td className={styles.td3}>
+                <p className={styles.p}>
+                  Gallons used since June 5th 2023.
+                </p>
+              </td>
+              <td className={styles.td3}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Blue,
+                  }}
+                  type="text"
+                  name="utilized"
+                  id="Answer"
+                  label="answers"
+                  readOnly
+                ></input>
+              </td>
+            </tr>
+            <tr>
+              <td className={styles.td3}>
+                <p className={styles.p}>
+                  Total gallons used sofar of the annual allotment (of 48,000)
+                </p>
+              </td>
+              <td className={styles.td3}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Blue,
+                  }}
+                  type="text"
+                  name="utilized"
+                  id="thisYear"
+                  value={data.june05_23 }
+                  label="answers"
+                  readOnly
+                ></input>
+              </td>
+            </tr>
 
-    </Zoom>
+            <tr>
+              <td className={styles.td3} function person>
+                <p className={styles.p}>
+                  Percentage annual allotment (48,000 gallons) used.
+                  <br />
+                </p>
+              </td>
+              <td className={styles.td3}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Blue,
+                  }}
+                  type="text"
+                  id="Percent"
+                  label="answers"
+                  readOnly
+                ></input>
+              </td>
+            </tr>
 
+            <tr>
+              <td className={styles.td3} function person>
+                <p className={styles.p}>
+                  End of billing year.
+                  <br />
+                </p>
+              </td>
+              <td className={styles.td3}>
+                <input
+                  style={{
+                    backgroundColor: bgColors.Blue,
+                  }}
+                  type="text"
+                  id="yearEnd1"
+                  value="June 2024"
+                  label="answers"
+                  readOnly
+                ></input>
+              </td>
+            </tr>
 
+            <tr>
+              <td className={styles.h5head}>
+                <h5>
+                  *If you are having problems with the
+                  <br />
+                  form, we recommend reading &quot;
+                  <a href="/howtoreadmeter.pdf">How to read your meter?</a>
+                  &quot;{" "}
+                </h5>
+              </td>
+              <td className={styles.td0}></td>
+            </tr>
+          </tbody>
+        </table>
+      </Zoom>
 
-
-  
-
-
-<style jsx>{`
-        
+      <style jsx>{`
         a {
           color: teal;
         }
       `}</style>
-      </>
-)};
+    </>
+  );
+}
