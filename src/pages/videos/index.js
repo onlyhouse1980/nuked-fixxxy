1a78bd482a954e04933e7421e6394ef77f5bf855
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-07-30 05:47:14 +0200

    upscaled videos

--- src/pages/videos/index.js
+++ src/pages/videos/index.js
@@ -1,84 +1,79 @@
-import styles from '../../styles/Whales.module.css'
-import Link from 'next/link'
-
-
+import styles from "../../styles/Whales.module.css";
+import Link from "next/link";
 
 const Whales = () => {
-  
-    const media_urls = [
-      {
-        id: 1,
-        title: 'Whales 1/3',
-        url: 'videos/video1',
-        video_url:
-          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501529/obcg/whale1_iyc5yc.mp4#t=0.001',
-      },
-      {
-        id: 2,
-        title: 'Whales 2/3',
-        url: 'videos/video2',        
-        video_url:
-          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501527/obcg/whale2_haymfa.mp4#t=0.001',
-      },
-      {
-        id: 3,
-        title: 'Whales 3/3',
-        url: 'videos/video3',
-        video_url:
-          'https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501527/obcg/whale3_ziwwhq.mp4#t=0.001',
-      },
-    ]
-  
-    // handle mouse enter
-    const handleMouseEnter = (e) => {
-      const vid = e.target
-      vid.muted = true
-      vid.play()
-    }
-  
-    // handle mouse leave
-    const handleMouseLeave = (e) => {
-      const vid = e.target
-      vid.muted = false
-      vid.currentTime = 0
-      vid.pause()
-    }
-  
-    return (
-      <>
+  const media_urls = [
+    {
+      id: 1,
+      title: "Whales 1/3",
+      url: "videos/video1",
+      video_url:
+        "https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501529/obcg/whale1_iyc5yc.mp4#t=0.001",
+    },
+    {
+      id: 2,
+      title: "Whales 2/3",
+      url: "videos/video2",
+      video_url:
+        "https://res.cloudinary.com/dfnaxhqqq/video/upload/ac_none,q_auto:best/v1690687830/whale22_kwxnuo.mp4#t=0.001",
+    },
+    {
+      id: 3,
+      title: "Whales 3/3",
+      url: "videos/video3",
+      video_url:
+        "https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501527/obcg/whale3_ziwwhq.mp4#t=0.001",
+    },
+  ];
+
+  // handle mouse enter
+  const handleMouseEnter = (e) => {
+    const vid = e.target;
+    vid.muted = true;
+    vid.play();
+  };
+
+  // handle mouse leave
+  const handleMouseLeave = (e) => {
+    const vid = e.target;
+    vid.muted = false;
+    vid.currentTime = 0;
+    vid.pause();
+  };
+
+  return (
+    <>
       <div className={styles.headline}>
-        <h1 className='text-center'>Pickering Passage, July 2022</h1>
-      </div>  
-        <div className='container'>
-          <div className='row'>
-            {media_urls.map((media) => (
-              <div key={media.id} className='col-lg-4 col-sm-6 mb-4'>
-                <Link href={media.url}>
-                <div className='card h-100 bg-indigo-500'>
+        <h1 className="text-center">Pickering Passage, July 2022</h1>
+      </div>
+      <div className="container">
+        <div className="row">
+          {media_urls.map((media) => (
+            <div key={media.id} className="col-lg-4 col-sm-6 mb-4">
+              <Link href={media.url} legacyBehavior>
+                <div className="card h-100 bg-indigo-500">
                   <div className={styles.cardBody}>
-                  <h4 className={styles.cardTitle}> {media.title}</h4>
+                    <h4 className={styles.cardTitle}> {media.title}</h4>
                     <video
                       preload="metadata"
-                      width='100%'
-                      height='auto'
-                      padding-bottom='10px'
+                      width="100%"
+                      height="auto"
+                      padding-bottom="10px"
                       controls
                       onMouseEnter={handleMouseEnter}
                       onMouseLeave={handleMouseLeave}
                     >
-                      <source src={media.video_url} type='video/mp4' />
+                      <source src={media.video_url} type="video/mp4" />
                     </video>
                   </div>
                 </div>
-                </Link>
-              </div>
-            ))}
-          </div>
+              </Link>
+            </div>
+          ))}
         </div>
-      </>
-    )
-  
-  }
-  
-  export default Whales
-  
+      </div>
+    </>
+  );
+};
+
+export default Whales;
--- src/pages/videos/video2.js
+++ src/pages/videos/video2.js
@@ -1,25 +1,32 @@
 // import VideoBg1 from '../components/VideoBg1'
-import styles from '../../styles/About.module.css'
-import { motion } from 'framer-motion'
-import Zoom from 'react-reveal/Zoom'
-
-
+import styles from "../../styles/About.module.css";
+import { motion } from "framer-motion";
+import Zoom from "react-reveal/Zoom";
 
 const Video2 = () => (
-<motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
-   
+  <motion.div
+    exit={{ opacity: 0 }}
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+  >
     <Zoom top>
-    <motion.div className={styles.content}
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ delay: 0.2 }}>
-      <video className='fixed z-\[-1\]' controls muted loop style={{ width: '100%', height: '100%' }}
-         src="https://res.cloudinary.com/dfnaxhqqq/video/upload/v1657501527/obcg/whale2_haymfa.mp4#t=0.001" />
-		 
-    </motion.div>
+      <motion.div
+        className={styles.content}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.2 }}
+      >
+        <video
+          className="fixed z-\[-1\]"
+          controls
+          muted
+          loop
+          style={{ width: "100%", height: "100%" }}
+          src="https://res.cloudinary.com/dfnaxhqqq/video/upload/ac_none,q_auto:best/v1690687830/whale22_kwxnuo.mp4#t=0.001"
+        />
+      </motion.div>
     </Zoom>
   </motion.div>
-
 );
 
 export default Video2;
