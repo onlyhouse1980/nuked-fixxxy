9c52bf03c74a2691044c0934c278ba1b92102da8
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-02 05:18:44 +0200

    Update sep172023min.js

--- src/pages/pdfs/sep172023min.js
+++ src/pages/pdfs/sep172023min.js
@@ -1,1 +1,20 @@
+import styles from "../../styles/pdfs.module.css";
+import React, { useState } from "react";
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/1Fj5pEnQhuoSXwU02ucB5fw2ATrq-tfpk/preview"
+  );
+  return (
+    <div className="container">
+      <h5>Consumer Confidence 2020</h5>
+      <iframe
+        className={styles.iframe}
+        src={embedURL}
+        width="100%"
+        height="500"
+      ></iframe>
+    </div>
+  );
+};
 
+export default App;
