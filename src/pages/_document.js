efd57eb89cc56592f49b3f8f054ab079a2df191f
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-13 12:56:14 +0200

    lang="en-US"

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html>
+    <Html lang="en-US">
       <Head>
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
