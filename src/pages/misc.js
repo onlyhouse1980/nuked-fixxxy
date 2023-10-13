371e8158acb05e2494e2efb1b226ae9515bb3bf3
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-13 15:09:04 +0200

    Update misc.js

--- src/pages/misc.js
+++ src/pages/misc.js
@@ -1,33 +1,13 @@
 import Link from 'next/link';
 import styles from '../styles/misc.module.css';
-import { motion } from 'framer-motion'
 
-export async function getStaticProps() {
-  return {
-    props: {},
-  };
-}
 
-        const container = {
-          hidden: { opacity: 0 },
-          show: {
-            opacity: 1,
-            transition: {
-              staggerChildren: 0.5
-            }
-          }
-        }
-        const item = {
-          hidden: { opacity: 0 },
-          show: { opacity: 1 }
-        }
-        const Misc = () => (
-          <motion.div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
+
+        export default function Misc() {
+          return(
+          <div exit={{ opacity: 0 }} initial={{opacity: 0 }} animate={{opacity: 1 }}>
            
-            <motion.container className={styles.bgcontainer}
-            variants={container}
-            initial="hidden"
-            animate="show" >
+            <div className={styles.bgcontainer}>
               <h2 className='text-center'><b>OBCG</b><br /><b>Historical Documents</b></h2>
               <p></p>
               <div className={styles.divBlock}>
@@ -39,7 +19,7 @@ export async function getStaticProps() {
                   <button className={styles.button}><Link  href='/pdfs/misc/historicalmeetings'>Assorted Documents</Link></button>
                 </div>
               </div>
-            </motion.container>
-          </motion.div>
-        );
-        export default Misc;
+            </div>
+          </div>
+        )};
+        
