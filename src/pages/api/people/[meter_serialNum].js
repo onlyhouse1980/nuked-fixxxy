bd873be74dae7faa1ef70104867aac70b02b58d4
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-13 17:16:15 +0200

    Update [meter_serialNum].js

--- src/pages/api/people/[meter_serialNum].js
+++ src/pages/api/people/[meter_serialNum].js
@@ -1,12 +1,19 @@
 import { people } from '../../../../data'
+import { sanitize } from 'some-sanitization-library';
 
 export default function personHandler({ query: { meter_serialNum } }, res) {
-  const filtered = people.filter((p) => p.meter_serialNum === meter_serialNum)
+  try {
+    const sanitizedMeterSerialNum = sanitize(meter_serialNum);
 
-  // User with meter_serialNum exists
-  if (filtered.length > 0) {
-    res.status(200).json(filtered[0])
-  } else {
-    res.status(404).json({ message: `User with meter_serialNum: ${meter_serialNum} not found.` })
+    const person = people.find((p) => p.meter_serialNum === meter_serialNum)
+
+    // User with meter_serialNum exists
+    if (person) {
+      res.status(200).json(person)
+    } else {
+      res.status(404).json({ message: `User with meter_serialNum: ${meter_serialNum} not found.` })
+    }
+  } catch (error) {
+    res.status(500).json({ message: "An error occurred while processing the request." })
   }
 }
