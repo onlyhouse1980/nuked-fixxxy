69fd72bfe88409b1c617744986fc81f5f122f0ff
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-13 17:38:17 +0200

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,6 +11,8 @@ export default function Layout(props) {
   return (
     <>
      <Head>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
       <title>{props.title}</title>
       <meta name="description" content={props.description}></meta>
       
