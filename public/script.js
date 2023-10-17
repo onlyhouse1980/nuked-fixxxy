cf70ae19128830227c313e9a654c25f7f6d952a0
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-17 16:08:21 +0200

    water effect test

---
+++ public/script.js
@@ -0,0 +1,25 @@
+let elements = document.querySelectorAll(".text");
+
+    elements.forEach((element) => {
+        let innerText = element.innerText;
+        element.innerHTML = "";
+
+        let textContainer = document.createElement("div");
+        textContainer.classList.add("block");
+
+        for (let letter of innerText) {
+            let span = document.createElement("span");
+            span.innerText = letter.trim() === "" ? "\xa0" : letter;
+            span.classList.add("letter");
+            textContainer.appendChild(span);
+        }
+
+        element.appendChild(textContainer);
+        element.appendChild(textContainer.cloneNode(true));
+    });
+
+    elements.forEach((element) => {
+        element.addEventListener("mouseover", () => {
+            element.classList.remove("play");
+    });
+});
