e226ed18a914e6770650c81513bb27ad185785fe
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2023-10-19 21:52:26 +0200

    Update [last_name].js

--- src/pages/api/overuse/[last_name].js
+++ src/pages/api/overuse/[last_name].js
@@ -1,16 +1,12 @@
 import { LastNames } from '../../../../data1'
 
 export default function overHandler({ query: { last_name } }, res) {
-  try {
-    const filtered = LastNames.filter((p) => p.last_name === last_name)
+  const filtered = LastNames.filter((p) => p.last_name === last_name)
 
-    // User with meter_serialNum exists
-    if (filtered.length > 0) {
-      res.status(200).json(filtered)
-    } else {
-      const sanitizedLastName = sanitizeInput(last_name);
-      res.status(404).json({ message: `User with last_name: ${sanitizedLastName} not found.` })    }
-  } catch (error) {
-    res.status(500).json({ message: 'An error occurred.' })
+  // User with meter_serialNum exists
+  if (filtered.length > 0) {
+    res.status(200).json(filtered[0])
+  } else {
+    res.status(404).json({ message: `User with last_name: ${last_name} not found.` })
   }
 }
