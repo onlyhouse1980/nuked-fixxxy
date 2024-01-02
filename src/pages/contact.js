1fc5b7f7708f86390a6e697743daf1a30089dd06
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-01-02 01:28:15 +0100

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -24,6 +24,17 @@ const email = `orchardwater@yahoo.com`;
     
     <container className={styles.content}>
     <h2 className={styles.h2Title}>Orchard Beach Established 1954</h2>
+    
+    <div className={styles.info}>
+      <div><h3>OBCG</h3></div>
+      <div >
+      Orchard Beach
+Grapeview, WA 98546 <br />
+      Email: 
+      
+      <button label="Write me an E-Mail"><a href = "${email}?&subject=OBCG%20Member%20Email">orchardwater@yahoo.com</a></button></div>
+      </div>
+
     <Image className={styles.mapImage}   
       src="/Images/WebPFiles/fullsmall.webp"
       alt="Schematic map of Orchard Beach" 
@@ -32,15 +43,7 @@ const email = `orchardwater@yahoo.com`;
       priority
       />
       
-      <div className={styles.info}>
-      <div ><h3>OBCG</h3></div>
-      <div >
-      Orchard Beach
-Grapeview, WA 98546 <br />
-      Email: 
       
-      <button label="Write me an E-Mail"><a href = "${email}?&subject=OBCG%20Member%20Email">orchardwater@yahoo.com</a></button></div>
-      </div>
     </container>
     
   </div>
