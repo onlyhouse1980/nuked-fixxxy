b1b38d123dabdf04836b73947d1c0f6349fc2383
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2024-02-06 16:01:27 +0100

    Create feb2024.js

---
+++ src/pages/pdfs/newsletters/feb2024.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import styles from "../../styles/pdfs.module.css";
+
+export const App = () => {
+  const [embedURL] = useState(
+    "https://drive.google.com/file/d/15BXMnZLRHUeqsYawWAHK4qr_i1LxSEF3/preview"
+  );
+  return (
+    <div className="container">
+      <iframe
+        className={styles.iframPDF}
+        src={embedURL}
+        width="100%"
+        height="1000px"
+      ></iframe>
+    </div>
+  );
+};
+
+export default App;
